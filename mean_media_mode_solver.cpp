21d0675988f184aeea763e5de29298fc5fba3c39
Aaron Maraya <aaronmaraya7@gmail.com>
2023-01-03 18:10:33 +0800

    -add for_mean function to solve for the mean value
    -mean_median_mode_solver.cpp file

--- mean_media_mode_solver.cpp
+++ mean_media_mode_solver.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void for_mean(float frequency[], float fx[], int num_of_class){
+    float sum_of_frequency;
+    float sum_of_fx;
+    for(int i = 0; i < num_of_class; i++){
+        sum_of_frequency += frequency[i];
+        sum_of_fx += fx[i];
+    }
+    cout << "\nn = " << sum_of_frequency << endl;
+    cout << "summation of fx = " << sum_of_fx << endl;
+
+    float mean = sum_of_fx / sum_of_frequency;
+    cout << "\nMean = " << mean << endl;
+
+}    
+
+
 int main(){
     int num_of_class;
     cout << "How many classes? ";
@@ -29,6 +45,8 @@ int main(){
         arr_for_cf_a[i] = arr_for_cf_a[i-1] + arr_frequency[i];
         cout << "cf = " << arr_for_cf_a[i] << endl;
     }
+
+    for_mean(arr_frequency, arr_for_fx, num_of_class);
 
     return 0;
 }
