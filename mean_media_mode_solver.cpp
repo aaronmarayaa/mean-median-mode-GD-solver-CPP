844bfd37e1f4f360a47051eb231f1d5e375c4a98
Aaron Maraya <aaronmaraya7@gmail.com>
2023-01-04 08:28:31 +0800

    -add for_median function to solve for the median value
    -mean_median_mode_solver.cpp file

--- mean_media_mode_solver.cpp
+++ mean_media_mode_solver.cpp
@@ -16,6 +16,42 @@ void for_mean(float frequency[], float fx[], int num_of_class){
 
 }    
 
+void for_median(float low_class[], float high_class[], float frequency[], int num_of_class, float cf[]){
+    float sum_of_frequency;
+    float n_divide_2;
+        for(int j = 0; j < num_of_class; j++){ //This will add all the frequency value
+            sum_of_frequency += frequency[j];
+        }
+
+    //This formula will determine which class is the median if it is under the scope of a certain cf
+    n_divide_2 = sum_of_frequency / 4; 
+    //This is divide by four since the for_median function takes the frequency value in the for+_mean function+
+    
+    float median;
+        //if n_divide_2 is less than the cf of the first class; then execute the body
+        if(n_divide_2 <= cf[0]){ 
+            float lmd = low_class[0] - 0.5;
+            cf[0];
+            float fmd = frequency[0];
+            float interval = high_class[0] + 1 - low_class[0];
+
+            median = (((n_divide_2 - cf[0]) / fmd) * interval);
+        }
+
+    cf[-1] = cf[0];
+    for(int i = 0; i < num_of_class; i++){
+        //this if statements will determine which set of class we should get every value we put in our formula
+        if(n_divide_2 > cf[i] && n_divide_2 <= cf[i + 1]){ 
+            float lmd = low_class[i + 1] - 0.5; // this is lmd
+            cf[i - 1]; // this is <cf
+            float fmd = frequency[i + 1]; //this is fmd
+            float interval = high_class[i] + 1 - low_class[i]; // this is the interval
+
+            median = lmd + (((n_divide_2 - cf[i]) / fmd) * interval);
+        }
+    }
+        cout << "Median = " << median << endl;
+}
 
 int main(){
     int num_of_class;
@@ -47,6 +83,7 @@ int main(){
     }
 
     for_mean(arr_frequency, arr_for_fx, num_of_class);
+    for_median(arr_low, arr_high, arr_frequency, num_of_class, arr_for_cf_a);
 
     return 0;
 }
