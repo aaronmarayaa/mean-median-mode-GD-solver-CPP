f3b63e633b8291cfb347e12121cf99aa7b0fe936
Aaron Maraya <aaronmaraya7@gmail.com>
2023-01-29 08:04:37 +0800

    Rename num_of_class variable to number_of_class
    -mean_median_mode_solver.cpp file

--- mean_media_mode_solver.cpp
+++ mean_media_mode_solver.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void for_mean(float frequency[], float fx[], int num_of_class){
+void for_mean(float frequency[], float fx[], int number_of_class){
     float sum_of_frequency = 0;
     float sum_of_fx = 0;
-    for(int i = 0; i < num_of_class; i++){
+    for(int i = 0; i < number_of_class; i++){
         sum_of_frequency += frequency[i];
         sum_of_fx += fx[i];
     }
@@ -16,10 +16,10 @@ void for_mean(float frequency[], float fx[], int num_of_class){
 
 }    
 
-void for_median(float low_class[], float high_class[], float frequency[], int num_of_class, float cf[]){
+void for_median(float low_class[], float high_class[], float frequency[], int number_of_class, float cf[]){
     float sum_of_frequency = 0;
     float n_divide_2;
-        for(int j = 0; j < num_of_class; j++){ //This will add all the frequency value
+        for(int j = 0; j < number_of_class; j++){ //This will add all the frequency value
             sum_of_frequency += frequency[j];
         }
 
@@ -38,7 +38,7 @@ void for_median(float low_class[], float high_class[], float frequency[], int nu
         }
 
     cf[-1] = cf[0];
-    for(int i = 0; i < num_of_class; i++){
+    for(int i = 0; i < number_of_class; i++){
         //this if statements will determine which set of class we should get every value we put in our formula
         if(n_divide_2 > cf[i] && n_divide_2 <= cf[i + 1]){ 
             float lmd = low_class[i + 1] - 0.5; // this is lmd
@@ -52,9 +52,9 @@ void for_median(float low_class[], float high_class[], float frequency[], int nu
         cout << "Median = " << median << endl;
 }
 
-int modal_class(float frequency[], int num_of_class){
+int modal_class(float frequency[], int number_of_class){
     int max = 0;
-    for(int i = 0; i < num_of_class; i++){
+    for(int i = 0; i < number_of_class; i++){
             if(max < frequency[i]){
                 max = frequency[i];
             }
@@ -62,7 +62,7 @@ int modal_class(float frequency[], int num_of_class){
     return max;
 }
 
-void for_mode(float frequency[], int num_of_class, float low_class[], float high_class[]){
+void for_mode(float frequency[], int number_of_class, float low_class[], float high_class[]){
     float lmo;
     float delta_one;
     float delta_two;
@@ -70,12 +70,12 @@ void for_mode(float frequency[], int num_of_class, float low_class[], float high
     float delta;
     float mode;
     frequency[-1] = 0;
-    for(int i = 0; i < num_of_class; i++){
+    for(int i = 0; i < number_of_class; i++){
     lmo = low_class[i] - 0.5;
     delta_one = frequency[i] - frequency[i - 1];
     delta_two = frequency[i] - frequency[i + 1];
     interval = high_class[i] + 1 - low_class[i];
-        if(modal_class(frequency, num_of_class) == frequency[i]){
+        if(modal_class(frequency, number_of_class) == frequency[i]){
             delta = delta_one + delta_two;
             mode = lmo + ((delta_one / delta) * interval);
         }
@@ -86,26 +86,31 @@ void for_mode(float frequency[], int num_of_class, float low_class[], float high
 int main(){
     cout << "\nThis is a solver for mean, median, and mode value.\n";
     cout << "Placing the set of classes should be from LOWEST to HIGHEST.\n\n";
-    int num_of_class;
+    int number_of_class;
     cout << "How many classes? ";
-    cin >> num_of_class;
-    float arr_low[num_of_class];
-    float arr_high[num_of_class];
-    float arr_frequency[num_of_class];
-    float arr_for_x[num_of_class];
-    float arr_for_fx[num_of_class];
-    float arr_for_cf[num_of_class];
+    cin >> number_of_class;
+    float arr_low[number_of_class];
+    float arr_high[number_of_class];
+    float arr_frequency[number_of_class];
+    float arr_for_x[number_of_class];
+    float arr_for_fx[number_of_class];
+    float arr_for_cf[number_of_class];
     arr_for_cf[-1] = 0;
-    for(int i = 0; i < num_of_class; i++){
+    for(int i = 0; i < number_of_class; i++){
         cout << "\nEnter low class value: ";
             cin >> arr_low[i];
+
         cout << "Enter high class value: ";
             cin >> arr_high[i];
-        cout << arr_low[i] << " - " << arr_high[i] << endl;  
+
+        cout << arr_low[i] << " - " << arr_high[i] << endl;
+        
         cout << "frequency: ";
             cin >> arr_frequency[i];
+
         arr_for_x[i] = (arr_low[i] + arr_high[i])/2;
         cout << "x = " << arr_for_x[i] << endl;
+        
         arr_for_fx[i] = arr_for_x[i] * arr_frequency[i];
         cout << "fx = " << arr_for_fx[i] << endl;
 
@@ -113,9 +118,9 @@ int main(){
         cout << "cf = " << arr_for_cf[i] << endl;
     }
 
-    for_mean(arr_frequency, arr_for_fx, num_of_class);
-    for_median(arr_low, arr_high, arr_frequency, num_of_class, arr_for_cf);
-    for_mode(arr_frequency, num_of_class, arr_low, arr_high);
+    for_mean(arr_frequency, arr_for_fx, number_of_class);
+    for_median(arr_low, arr_high, arr_frequency, number_of_class, arr_for_cf);
+    for_mode(arr_frequency, number_of_class, arr_low, arr_high);
 
 
     return 0;
